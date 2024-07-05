510e1c6b78d4197537b13204d68e0e4b3a509090
OskarAnderssons <oskar.simon.andersson@gmail.com>
2024-07-05 16:47:16 +0200

    Update plotting2.py

--- Testkod/Enskilda_delsystem/Orienteringssensor/Kalibreringstest/plotting2.py
+++ Testkod/Enskilda_delsystem/Orienteringssensor/Kalibreringstest/plotting2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # 1. File Path
-file_path = "your_data_file.txt"  # Replace with the correct path
+file_path = "Testkod\Enskilda_delsystem\Orienteringssensor\Kalibreringstest\Example9_DMP_MultipleSensors\data.txt"  # Replace with the correct path
 
 # 2. Read Data with Pandas (Assuming space-separated values and no header)
 data = pd.read_csv(file_path, sep=",", header=None, names=["X", "Y", "Z"])
